803f59fd70945fc1ace2d8c7b509d1b99d847f33
Ruchi-Kumari <152387455+ruchikumari-jha@users.noreply.github.com>
2025-11-30 16:04:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Palindromic Subsequences/count-palindromic-subsequences.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    int countPS(string &s) {
+        int n=s.length();
+        vector<vector<long long>> dp(n,vector<long long>(n,0));
+        
+        for(int i=0;i <n;i++){
+            dp[i][i]=1;
+        }
+        for( int len =2;len <=n;len++){
+            for(int i=0;i<=n-len;i++){
+                int j=i+len-1;
+                
+                if(s[i] == s[j]){
+                    dp[i][j]=dp [i+1][j]+dp[i][j-1]+1;
+                }
+                else{
+                    dp[i][j]=dp[i+1][j] +dp[i][j-1]-dp[i+1][j-1];
+                }
+            }
+        }
+        return dp[0][n-1];
+        
+    }
+};
